2874d2b56f392a6114a36181a13cb0e382f6b74c
gurk <gurkmandur@gmail.com>
2019-04-01 12:49:02 -0400

    Fixing the block screen page to overlap all elements and cover the entire page

--- block.js
+++ block.js
@@ -1,6 +1,17 @@
 //document.body.innerHTML = '<div id="block">This Page is Blocked! Get back to work, you can do it!</div>';
 
-document.getElementById('body').appendChild(document.createElement("div"))
+var div = document.createElement("div");
+div.id = 'block';
+div.innerHTML = "This Page is Blocked! Get back to work, you can do it!";
+document.body.appendChild(div);
+
+
+//var textnode = document.createTextNode("Water"); 
+//document.getElementById('body').appendChild(div);
+
+
+//document.body.prepend(div);
+
 
 /*
 document.open('text/html');
