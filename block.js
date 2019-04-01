580be27b6f7d61a8e8f98dd7d8dc58796adca2dc
gurk <gurkmandur@gmail.com>
2019-04-01 13:07:57 -0400

    only allow 1 block div element to be inserted

--- block.js
+++ block.js
@@ -1,9 +1,14 @@
 //document.body.innerHTML = '<div id="block">This Page is Blocked! Get back to work, you can do it!</div>';
 
-var div = document.createElement("div");
-div.id = 'block';
-div.innerHTML = "This Page is Blocked! Get back to work, you can do it!";
-document.body.appendChild(div);
+
+// if page is not already blocked
+if (!document.getElementById("block")) {
+	var div = document.createElement("div");
+	div.id = 'block';
+	div.innerHTML = "This Page is Blocked! Get back to work, you can do it!";
+	document.body.appendChild(div);
+}
+
 
 
 //var textnode = document.createTextNode("Water"); 
