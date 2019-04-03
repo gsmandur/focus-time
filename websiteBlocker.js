d54e7eba8ffd41594490da23c73ec1672ca94d09
gurk <gurkmandur@gmail.com>
2019-04-02 20:50:35 -0400

    seperated code into block and unblock functions

--- websiteBlocker.js
+++ websiteBlocker.js
@@ -3,43 +3,57 @@
 // handle new window, google search of new website (ie url changes)
 
 
+function unblockSite() {
+	chrome.tabs.executeScript(null, {file: 'unblock.js'});
+}
+
 function blockSite(tabId) {
+	// get tab url
+	chrome.tabs.get(tabId, function(tab) {
+	  console.log(tab.url);
+	  // block the page here
+	  var testUrl = "youtube.com";
+	  
+	  // TODO   
+	  // Check if url is on blacklist
+	  var isBlackListed = tab.url.includes(testUrl);
+
+	  // execute script does not work with chrome urls (and does not need to)
+	  //var isChromeUrl = tab.url.indexOf("chrome://") === 0 ? true : false;
+	  
+	  if (true) {
+			chrome.tabs.executeScript(null, {file: 'block.js'});
+			chrome.tabs.insertCSS(null, {file: 'block.css'});
+		}
+
+	});
+}
+
+//blocks or unblocks the current website
+function handleSite(tabId) {
 	// check if alarm is set
 	chrome.alarms.getAll(function(alarms) {
-		// dont block site if no alarm set or on break 
-		//if (alarms.length === 0 || alarms[0].name === 'breakTimer') return;	
-
-		// get tab url
-	  chrome.tabs.get(tabId, function(tab) {
-	    console.log(tab.url);
-	    // block the page here
-	    var testUrl = "youtube.com";
-	    
-	    // TODO   
-	    // Check if url is on blacklist
-	    var isBlackListed = tab.url.includes(testUrl);
-
-	    // execute script does not work with chrome urls (and does not need to)
-	    //var isChromeUrl = tab.url.indexOf("chrome://") === 0 ? true : false;
-	    
-	    if (true) {
-				chrome.tabs.executeScript(null, {file: 'block.js'});
-				chrome.tabs.insertCSS(null, {file: 'block.css'});
-			}
-
-	  });
+		// unblock site if no alarm set or on break 
+		if (alarms.length === 0 || alarms[0].name === 'breakTimer') {
+			unblockSite();
+		}	
+    else {
+			blockSite(tabId);
+    }
 	});
 }
 
 
 // when tab is updated (opened or refreshed)
 chrome.tabs.onUpdated.addListener(function(tabId) {
-	blockSite(tabId);
+	handleSite(tabId);
+	//chrome.tabs.executeScript(null, {file: 'unblock.js'});
+
 });
 
 // when user switches tabs
 chrome.tabs.onActivated.addListener(function(activeInfo) {
-	blockSite(activeInfo.tabId);
+	handleSite(activeInfo.tabId);
 });
 
 
