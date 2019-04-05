a5a4dd11d7f98f9965a1dd6b8a1e060c1c84158e
gurk <gurkmandur@gmail.com>
2019-04-04 23:31:44 -0400

    Display blacklisted urls and allow user to remove them

--- options.js
+++ options.js
@@ -1,32 +1,86 @@
+// removes url from the blacklist array
+function whiteListSite(url) {
+  // get the blacklist from storage
+  chrome.storage.local.get({
+    blackList: [] // set if not defined
+  }, function(item) {
+
+    // remove url from the BL
+    let newBL = item.blackList; 
+    for (var i=newBL.length-1; i>=0; i--) {
+      if (newBL[i] === url) {
+        newBL.splice(i, 1);
+      }
+    }
+    console.log(newBL);
+
+    // update storage
+    chrome.storage.local.set({
+      blackList: newBL  
+    });
+  
+  });
+}
+
+// add url of the blacklisted website on the options display
+function addSiteToDisplay(url) {
+
+  let list = document.getElementById('blackList');
+  let li = document.createElement('li');
+  li.innerHTML = url;
+
+  // 'X' for deleting site
+  let del = document.createElement('div');
+  del.class = "deleteMe";
+  del.innerHTML = "X";
+  
+  li.appendChild(del);
+  list.appendChild(li);
+
+  // add on click listener so clicking the 'X' deletes removes the site
+  del.onclick = function() {
+    // remove site from display
+    listElement = this.parentNode;
+    listElement.parentNode.removeChild(listElement);
+
+    // remove from storage
+    whiteListSite(url);
+  }
+
+
+}
 
 function blackListSite(url) {
   if (url === '') return;
-
 
   // get the blacklist from storage
   chrome.storage.local.get({
     blackList: [] // set if not defined
   }, function(item) {
 
-    // add url to BL
     let arr = item.blackList; 
-    if (!arr.includes(url)) { // make sure url is not already there
+    // add url to BL (only if not already there)    
+    if (!arr.includes(url)) { 
       arr.push(url);
       console.log(arr);
+      
+      // update storage
+      chrome.storage.local.set({
+        blackList: arr  
+      }, function() {
+        addSiteToDisplay(url);
+      });
     }
     
-    // update storage
-    chrome.storage.local.set({
-      blackList: arr  
-    });
 
   });
 
 
 }
 
-let urlTextArea = document.getElementById("url");
 
+
+let urlTextArea = document.getElementById("url");
 
 // users presses enter on text box
 urlTextArea.addEventListener("keyup", function(event) {
@@ -34,13 +88,7 @@ urlTextArea.addEventListener("keyup", function(event) {
     url = urlTextArea.value;
     console.log(url);
     urlTextArea.value = '';
-
-    // add to blacklisted websites
-    if (url !== '') {
-      blackListSite(url);
-
-
-    }
+    blackListSite(url);
     return false;
   }
 });
@@ -53,7 +101,7 @@ urlTextArea.addEventListener("keyup", function(event) {
 function save_options() {
   var color = document.getElementById('color').value;
   var likesColor = document.getElementById('like').checked;
-  chrome.storage.sync.set({
+  chrome.storage.local.set({
     favoriteColor: color,
     likesColor: likesColor
   }, function() {
@@ -70,7 +118,7 @@ function save_options() {
 // stored in chrome.storage.
 function restore_options() {
   // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get({
+  chrome.storage.local.get({
     favoriteColor: 'red',
     likesColor: true
   }, function(items) {
@@ -81,4 +129,8 @@ function restore_options() {
 
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click',save_options);
+
+
+//chrome.storage.local.clear();
+
 
