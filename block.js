188761a6a17dbdab47e46aace4ba8e7cccc22cec
gurk <gurkmandur@gmail.com>
2019-04-18 16:18:34 -0400

    multiple "website is blocked" prompts chosen randomly

--- block.js
+++ block.js
@@ -5,8 +5,15 @@
 if (!document.getElementById("block")) {
 	var div = document.createElement("div");
 	div.id = 'block';
-	// div.innerHTML = "This Page is Blocked! Get back to work, you can do it!";
-	div.innerHTML = "you: I want to block this site to focus. also you: nvm";
+	// div.innerHTML = 
+	let blockMessages = [
+		"This Page is Blocked! Get back to work, you can do it!",
+		"Think about how far you've gotten, dont't give in now!",
+		"Fancy meeting you here. Please just wait a little longer!",
+		"you: I want to block this site to focus. also you: nvm"
+	]
+	// display random message to user
+	div.innerHTML = blockMessages[Math.floor(Math.random() * Math.floor(blockMessages.length))];
 	document.body.appendChild(div);
 
 	var footer = document.createElement("div");
